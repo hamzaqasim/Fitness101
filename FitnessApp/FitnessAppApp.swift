394169a8feaa97f1eecacccb0cea14b79e5020de
hamza <hamzaqasimdgk@gmail.com>
2026-01-07 15:46:52 -0500

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Ali Hader on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
